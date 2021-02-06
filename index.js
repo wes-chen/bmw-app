d278fc17932498bd8db15676d54faa414fad215d
mylinhlac <>
2021-02-06 11:20:27 -0800

    Creating basic chat function
    Implemented socket.io and now messages show up in the chat log wow

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
