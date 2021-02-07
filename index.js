1fd13563b9f279cf9d41e7a6336177b3e56be32e
Wesley Chen <35469114+wes-chen@users.noreply.github.com>
2021-02-06 23:50:57 -0800

    just cleanup and reformat

--- index.js
+++ index.js
@@ -8,18 +8,16 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-  });
-  socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 });
-
-
 
 http.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
