f84a211d4d3cd3339d6050066082270efcdf8e02
Wesley Chen <35469114+wes-chen@users.noreply.github.com>
2021-03-08 16:53:25 -0800

    add rerecord option (#35)
    * add rerecord option
    
    * change id bacjk

--- public/audiorecord.js
+++ public/audiorecord.js
@@ -7,8 +7,9 @@ recordHello.onclick = function() {
     var url = URL.createObjectURL(blob);
     window.localStorage.setItem('helloURL', url);
     window.localStorage.setItem('helloSet', "SET");
-    $("#recordButtonHello").css("background-color", "#CDFFB6");
-    $("#recordButtonHello").text("Recorded");
+    $("#recordButtonHello").css("background-color", "#AFADFF");
+    $("#recordButtonHello").text("Rerecord");
+    $("#descnorm").text("'Hello' recorded!");
     $(".defaultrecord").css("color","white")
     $(".defaultrecord").css("border-color","white")
     if (window.localStorage.getItem('urgentSet') === "SET"
@@ -25,10 +26,11 @@ recordUrgent.onclick = function() {
     var url = URL.createObjectURL(blob);
     window.localStorage.setItem('urgentURL', url);
     window.localStorage.setItem('urgentSet', "SET");
-    $("#recordButtonUrgent").css("background-color", "#CDFFB6");
-    $("#recordButtonUrgent").text("Recorded");
-    $(".urgentrecord").css("color","white")
-    $(".urgentrecord").css("border-color","white")
+    $("#recordButtonUrgent").css("background-color", "#AFADFF");
+    $("#recordButtonUrgent").text("Rerecord");
+    $("#descurg").text("'Urgent' recorded!");
+    $(".urgentrecord").css("color","white");
+    $(".urgentrecord").css("border-color","white");
     if (window.localStorage.getItem('helloSet') === "SET"
       && window.localStorage.getItem('fbSet') === "SET"){
         $("#enter").css("background-color","white")
