b4ec84ecbe780e1021616e43dcb63b45ee4554fa
Wesley Chen <35469114+wes-chen@users.noreply.github.com>
2021-03-10 17:25:06 -0800

    try appearance properties

--- public/facebook.js
+++ public/facebook.js
@@ -3,21 +3,21 @@ function clearSet(){
   window.localStorage.clear();
   window.localStorage.setItem('muted', "FALSE");
 
-  // var source = document.getElementById("descriptiontemplate").innerHTML;
-  // compile the template
-  // var template = Handlebars.compile(source);
-  // // execute the compiled template and print the output to the console
-  // var context = {
-  //   descriptions: [{
-  //       description: "Get message notifications with sounds unique to each user."
-  //     },
-  //     {
-  //       description: "Never miss an urgent message again."
-  //     }
-  //   ]
-  // };
-  // html = template(context);
-  // $("#descriptionarea").append(html);
+  var source = document.getElementById("descriptiontemplate").innerHTML;
+  compile the template
+  var template = Handlebars.compile(source);
+  // execute the compiled template and print the output to the console
+  var context = {
+    descriptions: [{
+        description: "Get message notifications with sounds unique to each user."
+      },
+      {
+        description: "Never miss an urgent message again."
+      }
+    ]
+  };
+  html = template(context);
+  $("#descriptionarea").append(html);
 }
 
 function checkLoginState(){
